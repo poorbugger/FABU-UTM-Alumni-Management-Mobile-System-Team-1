31d0a83d15f1c4a4a67a0b918baae2074486804a
ryan-dono <adoryanjunior28@gmail.com>
2021-01-14 22:36:40 +0800

    changed Login.dart

--- lib/Login.dart
+++ lib/Login.dart
@@ -69,7 +69,7 @@ class _LoginPageState extends State<LoginPage> {
                   // ignore: deprecated_member_use
                   child:RaisedButton(
                     disabledElevation: 4.0,
-                    onPressed: null,
+                    onPressed: () {},
                     child: Text("Login", style: TextStyle(fontSize: 20,color:
                     Colors.white)),
                   ),
