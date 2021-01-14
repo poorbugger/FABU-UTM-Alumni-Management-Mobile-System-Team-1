171354e5d2af130ad4e4cb636862e07ce35ff974
poorbugger <69106288+poorbugger@users.noreply.github.com>
2021-01-14 23:46:07 +0800

    Update landing_page.dart

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -86,26 +86,6 @@ class Landing extends StatelessWidget {
               ),
 
 
-
-                    ),
-                  ),
-                ],
-              ),
-
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    flex:1,
-                    child: RaisedButton(
-                      color: Colors.indigo[300],
-                      onPressed: () {Navigator.pushNamed(context, '/logactivity');},
-                      child: Text('My activities'),
-
-                    ),
-                  ),
-                ],
-              ),
-
             ],
           ),
         ),
